60c6f2cbb7ed0d070b7884eb256537513452a14c
Jack Jennings <j@ckjennin.gs>
2014-09-13 08:44:30 -0700

    add basic parser features

--- smorgasbord/language_parser.py
+++ smorgasbord/language_parser.py
@@ -1,8 +1,29 @@
 from os.path import splitext, basename
+from itertools import takewhile
 
 class LanguageParser(object):
 
-    def __init__(self, language_file):
-        self.code = splitext(basename(language_file))[0]
-        self.name = 'Foo'
-        self.characters = []
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.code = splitext(basename(filepath))[0]
+        self.characters = self.parse_characters()
+        self.headers = self.parse_headers()
+        self.name = self.headers.get('Language')
+
+    def parse_characters(self):
+        with open(self.filepath) as f:
+            characters = [char for line in f if not self._is_comment(line)
+                               for char in line.rstrip('\n').split(' ')]
+        return characters
+
+    def _is_comment(self, line):
+        return line[0] == '#'
+
+    def parse_headers(self):
+        headers = {}
+        with open(self.filepath) as f:
+            head = takewhile(self._is_comment, f)
+            for l in head:
+                (key, value) = l[2:].split(':')
+                headers[key.lower()] = value.strip()
+        return headers
--- test/test_language_parser.py
+++ test/test_language_parser.py
@@ -4,8 +4,23 @@ from smorgasbord.language_parser import LanguageParser
 path = os.path.dirname(os.path.abspath(__file__))
 fixtures_path = os.path.join(path, 'fixtures')
 
+def make_parser(filename):
+    return LanguageParser(os.path.join(fixtures_path, filename))
+
 class TestLanguageParser(object):
 
     def test_parses_code(self):
-        parser = LanguageParser(os.path.join(fixtures_path, 'multiline.txt'))
+        parser = make_parser('multiline.txt')
         assert parser.code == 'multiline'
+
+    def test_parses_characters(self):
+        parser = make_parser('basic.txt')
+        assert [u"a", u"b", u"c"] == parser.characters
+
+    def test_parses_characters_from_multiple_lines(self):
+        parser = make_parser('multiline.txt')
+        assert [u"a", u"b", u"c"] == parser.characters
+
+    def test_parses_headers(self):
+        parser = make_parser('basic.txt')
+        assert {"language": "Foo"} == parser.headers
